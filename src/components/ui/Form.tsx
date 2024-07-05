779c7f110daede63eef47a763c733ffc1fad1e8a
Alexis <AlexDdevs@gmail.com>
2024-07-05 14:29:03 +0200

    Change breakpoint in Form

--- src/components/ui/Form.tsx
+++ src/components/ui/Form.tsx
@@ -1,8 +1,8 @@
 export default function Form({ content }: { content: string }) {
     return (
-        <form className="flex items-center gap-[10px] h-[50px] mb-5 max-sm:h-10 max-sm:gap-2 max-[425px]:h-[35px]">
-            <input type="text" placeholder="Your best email address" className="font-bodyFont font-medium text-sm text-black/50 bg-[#F7F7F7] rounded-lg h-full pl-5 w-[300px] max-sm:w-[200px] max-sm:text-sm max-sm:rounded-md max-[425px]:w-[175px] max-[425px]:text-[10px]" />
-            <button className="text-white font-semiBold bg-[#314EE7] rounded-lg py-[15px] px-5 w-[150px] h-full flex items-center justify-center max-sm:w-[100px] max-sm:text-xs max-sm:rounded-md max-[425px]:w-[85px] max-[425px]:text-[10px]">{content}</button>
+        <form className="flex items-center gap-[10px] h-[50px] mb-5 max-md:h-10 max-md:gap-2 max-[425px]:h-[35px]">
+            <input type="text" placeholder="Your best email address" className="font-bodyFont font-medium text-sm text-black/50 bg-[#F7F7F7] rounded-lg h-full pl-5 w-[300px] max-md:w-[200px] max-md:text-sm max-md:rounded-md max-[425px]:w-[175px] max-[425px]:text-[10px]" />
+            <button className="text-white font-semiBold bg-[#314EE7] rounded-lg py-[15px] px-5 w-[150px] h-full flex items-center justify-center max-md:w-[100px] max-md:text-xs max-md:rounded-md max-[425px]:w-[85px] max-[425px]:text-[10px]">{content}</button>
         </form>
     )
 }
